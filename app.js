cbe3d4ef260b8d76210f134a156ca1ac6a4c74a1
Nicolás <nmirabets@me.com>
2021-07-31 14:22:17 +0200

    cookie-parser uninstall

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -28,7 +27,6 @@ async function setupApp() {
 	app.use(logger('dev'));
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
-	app.use(cookieParser());
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.set('trust proxy', 1);
 	app.use(
