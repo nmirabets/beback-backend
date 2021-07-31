08f7f0a7453c15b0f2da875fbce76e412292edff
Nicolás <nmirabets@me.com>
2021-07-31 10:44:40 +0200

    added cookies samesite config

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ async function setupApp() {
 	app.use(express.urlencoded({ extended: false }));
 	app.use(cookieParser());
 	app.use(express.static(path.join(__dirname, 'public')));
+	app.set('trust proxy', 1);
 	app.use(
 		session({
 			store: MongoStore.create({
@@ -41,6 +42,8 @@ async function setupApp() {
 			saveUninitialized: true,
 			cookie: {
 				maxAge: 24 * 60 * 60 * 1000,
+				sameSite: process.env.COOKIES_SAMESITE === 'true' ? 'lax' : 'none',
+				secure: process.env.COOKIES_SAMESITE !== 'true',
 			},
 		})
 	);
